58bfddee2c76bf762d08477843517cb0b9c4470b
Zach Kelling <z@zeekay.io>
2024-06-24 13:21:09 +0800

    @luxfi/cloud 0.1.3

--- packages/cloud/rollup.config.mjs
+++ packages/cloud/rollup.config.mjs
@@ -1,44 +1,45 @@
-/*
- * @Author: Jason
- * @Description:
- */
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import del from 'rollup-plugin-delete';
-//import ttypescript from 'ttypescript';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
+export default [
+  {
     input: 'src/index.ts',
     output: {
-        dir: 'dist',
-        format: 'esm',
-        sourcemap: process.env.BUILD === 'production' ? false : true,
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: process.env.BUILD === 'production' ? false : true,
     },
     plugins: [
-        del({ targets: 'dist/*' }),
-        typescript({
-            //typescript: ttypescript,
-            tsconfig: 'tsconfig.json',
-            tsconfigOverride: {
-                exclude: ['./test/**'],
-                compilerOptions: {
-                    rootDir: '.',
-                },
-            },
-        }),
-        json(),
-        commonjs(),
+      del({ targets: 'dist/*' }),
+      typescript({ tsconfig: './tsconfig.json' }),
+      json(),
+      commonjs(),
     ],
     external: [
-        'luxnet',
-        'luxnet/dist/utils',
-        'luxnet/dist',
-        'ethers',
-        'web3',
-        'url',
-        'events',
-        '@openzeppelin/contracts/build/contracts/ERC20.json',
-        'xss',
+      'luxnet',
+      'luxnet/dist/utils',
+      'luxnet/dist',
+      'ethers',
+      'web3',
+      'url',
+      'events',
+      '@openzeppelin/contracts/build/contracts/ERC20.json',
+      '@openzeppelin/contracts/build/contracts/ERC721.json',
+      '@ethereumjs/tx',
+      '@ethereumjs/common',
+      'bip32',
+      'tiny-secp256k1',
     ],
-};
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
